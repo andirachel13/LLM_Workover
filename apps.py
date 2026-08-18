65bcb2120dc0d16e27e87ce54b6a98b8555c799f
Andi Rachel Azra Aqilah <andi.rachel13@gmail.com>
2026-08-18 12:01:23 +0700

    apps.py

--- apps.py
+++ apps.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 import pandas as pd
 import re
+from typing import Dict
 from config import Config
 from data_processor import DataProcessor
 from analytics import DataAnalyzer
